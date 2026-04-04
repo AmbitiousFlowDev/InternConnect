963080c9967b2f8001b04d6536e50759ec5b3fcd
AmbitiousFlowDev <mohamed.lafrouh@gmail.com>
2026-04-04 20:40:32 +0100

    Initial Repo

---
+++ src/main/java/uca/github/org/InternConnectApplication.java
@@ -0,0 +1,12 @@
+package uca.github.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class InternConnectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InternConnectApplication.class, args);
+	}
+}
