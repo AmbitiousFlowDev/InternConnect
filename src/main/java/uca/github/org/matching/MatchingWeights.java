2d3049c488fac41bc9d5c9edc73840ae38d4082e
AmbitiousFlowDev <mohamed.lafrouh@gmail.com>
2026-05-25 13:06:43 +0100

    UCA-188 Define offer-candidate matching criteria

---
+++ src/main/java/uca/github/org/matching/MatchingWeights.java
@@ -0,0 +1,14 @@
+package uca.github.org.matching;
+
+public final class MatchingWeights {
+
+    public static final int SKILLS = 50;
+    public static final int FIELD = 20;
+    public static final int LEVEL = 15;
+    public static final int LOCATION = 10;
+    public static final int AVAILABILITY = 5;
+    public static final int MIN_RELEVANT_SCORE = 50;
+
+    private MatchingWeights() {
+    }
+}
